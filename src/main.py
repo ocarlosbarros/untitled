3b72b285e981cbe50b9244891b495b4784784a79
Carlos Barros <carlos_dbs@hotmail.com>
2026-09-02 14:11:52 -0300

    feat(main):modify the main file to change the screen state based on the context

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 import pygame
-import sys
+from screens.screen import Screen
 
 pygame.init()
 
-FULL_HD_RESOLUTION = (1920, 1080)
-BACKGROUND_COLOR = (0,0,0)
+# Screen config
+HD_RESOLUTION = (1280, 720)
+BGCOLOR_MENU = (77,77,77)
+BGCOLOR_GAMEPLAY = (0, 0, 0)
 
-screen = pygame.display.set_mode(FULL_HD_RESOLUTION)
+screen = Screen("Untitled Game", HD_RESOLUTION)
+
+#Screen Manager
+context = 2
+
+
 
 running = True
-while (running):
+while running:
+
+    if(context == 1):
+        screen.show(BGCOLOR_MENU)
+    else:
+        screen.show(BGCOLOR_GAMEPLAY)
+
+    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill(BACKGROUND_COLOR)
-
-    pygame.display.flip()
-
-pygame.quit()
-sys.exit()
+            pygame.quit()
